12ff340a943185d3fb3bd94f385d9355b2f0a0a1
DIMA_w <oddff@mail.ru>
2023-08-28 16:19:20 +0300

    Анимация H1

---
+++ js/anim.js
@@ -0,0 +1,7 @@
+const sectionStart_h1 = document.querySelector(".sectionStart-h1");
+console.log(sectionStart_h1);
+function animActivH1() {
+  sectionStart_h1.classList.add("animActiv");
+}
+
+animActivH1();
